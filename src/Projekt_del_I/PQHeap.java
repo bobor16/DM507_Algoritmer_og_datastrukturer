6c0fc9870c6048282e7756121408cee27b8db967
Borgar Bordoy <bobor16@student.sdu.dk>
2019-03-17 21:35:04 +0100

    comment

--- src/Projekt_del_I/PQHeap.java
+++ src/Projekt_del_I/PQHeap.java
@@ -38,28 +38,38 @@ public class PQHeap implements PQ {
         return (2 * i) + 1;
     }
 
-    // Minimum heapifies the ArrayList. Finds the smallest key of a parent and the children's keys.
-    // If the smallest is not the parent key, the smallest child is exchanged with its parent.
+    /**
+     * Minimum heapifies the ArrayList. Finds the smallest key of a parent and
+     * the children's keys. If the smallest is not the parent key, the smallest
+     * child is exchanged with its parent.
+     */
     private void minHeapify(int i) {
         int left = left(i);
         int right = right(i);
         int min;
 
-        // The if statement checks if the left child is smaller than its parent,
-        // and assigns the smaller of the two to "min".
+        /* The if statement checks if the left child is smaller than its parent,
+        * and assigns the smaller of the two to "min".
+         */
         if (left <= list.size() - 1 && list.get(left).getKey() < list.get(i).getKey()) {
             min = left;
         } else {
             min = i;
         }
-        // The if statement checks if the right child is smaller than its parent, if true it will be assigned to "min".
-        // To clarify the right child compares itself with the  parent which can be the former left child or the parent "i",
-        // depending on what the above if statement has done.
+        /**
+         * The if statement checks if the right child is smaller than its
+         * parent, if true it will be assigned to "min". To clarify the right
+         * child compares itself with the parent which can be the former left
+         * child or the parent "i", depending on what the above if statement has
+         * done.
+         */
         if (right <= list.size() - 1 && list.get(right).getKey() < list.get(min).getKey()) {
             min = right;
         }
-        // This if statement swaps the parent with the smallest child.
-        // If the new parent is not the smallest, it makes a recursive call.
+        /**
+         * This if statement swaps the parent with the smallest child. If the
+         * new parent is not the smallest, it makes a recursive call.
+         */
         if (min != i) {
             Element temp = list.get(i);
             list.set(i, list.get(min));
@@ -73,8 +83,11 @@ public class PQHeap implements PQ {
         return list.get(1);
     }
 
-    // Extracts first element in queue and moves the last element to be the first one.
-    // Lastly the heapify method is called which min-heapifies the ArrayList and min(minimum) is returned.
+    /**
+     * Extracts first element in queue and moves the last element to be the
+     * first one. Lastly the heapify method is called which min-heapifies the
+     * ArrayList and min(minimum) is returned.
+     */
     @Override
     public Element extractMin() {
         Element min = min();
@@ -83,17 +96,23 @@ public class PQHeap implements PQ {
         minHeapify(1);
         return min;
     }
-    
-    //Inserts elements to the list, while managing a heap.
-    // Inserts an element e into the priority-tree whilst also maintains the min-heap tree
+
+    /**
+     * Inserts an element e into the priority-tree whilst also maintains the
+     * min-heap tree.
+     */
     @Override
     public void insert(Element e) {
         list.add(e);
         int i = list.size() - 1;
+        /**
+         * The while loop maintains the heap by switching the element e with its
+         * parent, making e the new parent, from there is compares the next
+         * parent to e and if e is smaller than the new parent it will be
+         * switched again, until e has a parent that is smaller or e is at the
+         * root.
+         */
 
-        // The while loop maintains the heap by switching the element e with its parent,
-        // making e the new parent, from there is compares the next parent to e and if e is smaller
-        // than the new parent it will be switched again, until e has a parent that is smaller or e is at the root.
         while (i > 1 && list.get(parent(i)).getKey() > list.get(i).getKey()) {
             Element temp = list.get(i);
             list.set(i, list.get(parent(i)));
