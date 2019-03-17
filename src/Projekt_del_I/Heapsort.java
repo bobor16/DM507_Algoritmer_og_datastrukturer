df0b2d4fea668490e45ea2ec0b1abb6c5691ca48
Borgar Bordoy <bobor16@student.sdu.dk>
2019-03-17 20:34:44 +0100

    sssss

--- src/Projekt_del_I/Heapsort.java
+++ src/Projekt_del_I/Heapsort.java
@@ -25,7 +25,5 @@ public class Heapsort {
             System.out.println(pq.extractMin().getKey());
             n--;
         }
-
     }
-
 }
--- src/Projekt_del_I/PQHeap.java
+++ src/Projekt_del_I/PQHeap.java
@@ -1,90 +1,92 @@
+
+import Projekt_del_I.Element;
+import Projekt_del_I.PQ;
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Projekt_del_I;
-
-import java.util.ArrayList;
-
 /**
  *
  * @author Borgar Bordoy
  */
 public class PQHeap implements PQ {
 
-    private ArrayList<Element> list = new ArrayList<>();
+    private ArrayList<Element> list;
 
     public PQHeap(int maxElms) {
         this.list = new ArrayList<>(maxElms + 1);
         this.list.add(null);
-
-    }
-    // Calculates index of parent
-    public int parent(int i) {
-        return (i / 2);
-    }
-    // Calculates index of parents left child
-    public int left(int i) {
-        return (i * 2);
-    }
-    // Calculates index of parents right child
-    public int right(int i) {
-        return ((i * 2) + 1);
     }
 
-    private Element minimum() {
+    private int parent(int i) {
+        return i / 2;
+    }
+
+    private int left(int i) {
+        return (2 * i);
+    }
+
+    private int right(int i) {
+        return (2 * i) + 1;
+    }
+
+    private void swap(int fpos, int spos) {
+//        Element temp = list.get(spos);
+//        list.set(spos, list.get(fpos));
+//        list.set(fpos, temp);
+    }
+
+    private void heapify(int i) {
+        int left = left(i);
+        int right = right(i);
+        int smallest;
+
+        if (left <= list.size() - 1 && list.get(left).getKey() < list.get(i).getKey()) {
+            smallest = left;
+        } else {
+            smallest = i;
+        }
+        if (right <= list.size() - 1 && list.get(right).getKey() < list.get(smallest).getKey()) {
+            smallest = right;
+        }
+        if (smallest != i) {
+//            swap(smallest, i);
+            Element temp = list.get(i);
+            list.set(i, list.get(smallest));
+            list.set(smallest, temp);
+            heapify(smallest);
+        }
+    }
+
+    private Element min() {
         return list.get(1);
     }
 
-    // Extracts first element in queue
     @Override
     public Element extractMin() {
-        Element min = minimum();
+        Element min = min();
         list.set(1, list.get(list.size() - 1));
         list.remove(list.size() - 1);
-        minHeapify(1);
-        minHeapify(1);   
+        heapify(1);
         return min;
     }
 
-     
     @Override
     public void insert(Element e) {
         list.add(e);
         int i = list.size() - 1;
 
         while (i > 1 && list.get(parent(i)).getKey() > list.get(i).getKey()) {
+
+//            swap(list.get(parent(i)).getKey(), i);
             Element temp = list.get(i);
             list.set(i, list.get(parent(i)));
             list.set(parent(i), temp);
             i = parent(i);
         }
     }
-    // Swaps the parent with the smallest child
-    public void swapper(int i, int parent) {
-        Element temp = list.get(parent);
-        list.set(parent, list.get(i));
-        list.set(i, temp);
-    }
-    
-    public void minHeapify(int i) {
-        int l = left(i);
-        int r = right(i);
-        int min;
 
-        if (l <= list.size() - 1 && list.get(l).getKey() < list.get(i).getKey()) {
-            min = l;
-        } else {
-            min = i;
-        }
-        if (r <= list.size() - 1 && list.get(r).getKey() < list.get(i).getKey()) {
-            min = r;
-        }
-        // Checks if parent is not the smallest
-        if (min != i) {
-            swapper(i, min);
-            minHeapify(min);
-        }
-    }
 }
