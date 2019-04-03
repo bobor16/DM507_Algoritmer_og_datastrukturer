2aedb7225104b60d812b7e1c976b08de2a9117dc
Borgar Bordoy <bobor16@student.sdu.dk>
2019-04-03 10:56:08 +0200

    skrrrrr
    I DONT KNOW WHAT THE FUCK IM DOOOOING

--- src/projekt_del_2/DictBinTree.java
+++ src/projekt_del_2/DictBinTree.java
@@ -5,40 +5,42 @@
  */
 package projekt_del_2;
 
-import java.util.HashMap;
-
 /**
  *
  * @author Borgar Bordoy
  */
 public class DictBinTree implements Dict {
 
-    private int key = 0;
-    private int index = 0;
+    private Tree tree;
+    private Node node;
 
     public DictBinTree() {
-        index++;
+    }
+
+    public boolean isEmpty() {
+        return (this.node == null);
     }
 
     @Override
     public void insert(int k) {
-        Node x = null;
+
         Node y = null;
+        Node x = new Node(k);
 
         while (x != null) {
             y = x;
             if (k < x.getKey()) {
-                x = x.getLeft();
+                x = x.getLeftChild();
             } else {
-                x = x.getRight();
+                x = x.getRightChild();
             }
-            k = y.getKey();
+            x.setParent(x);
             if (y == null) {
                 k = x.getKey();
             } else if (k < y.getKey()) {
-                k = y.getLeft().getKey();
+                k = y.getLeftChild().getKey();
             } else {
-                k = y.getRight().getKey();
+                k = y.getRightChild().getKey();
             }
 
         }
@@ -57,9 +59,78 @@ public class DictBinTree implements Dict {
             return true;
         }
         if (k < node.getKey()) {
-            return search(node.getLeft().getKey());
+            return search(node.getLeftChild().getKey());
         } else {
-            return search(node.getRight().getKey());
+            return search(node.getRightChild().getKey());
+
         }
     }
+}
+
+class Node {
+
+    private int key;
+    private Node left;
+    private Node right;
+    private Node parent;
+
+    public Node getParent() {
+        return parent;
+    }
+
+    public Node(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public Node getLeftChild() {
+        return left;
+    }
+
+    public Node getRightChild() {
+        return right;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public void setParent(Node parent) {
+        this.parent = parent;
+    }
+
+}
+
+class Tree {
+
+    Node root;
+    private int size;
+
+    public Node getRoot() {
+        return root;
+    }
+
+    public void setRoot(Node root) {
+        this.root = root;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
 }
