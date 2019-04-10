654e2db87994b6876d71d147e3d4d12d4e51dcd0
Martin <martin@wirksomhed.dk>
2019-04-10 14:25:55 +0200

    TreeSort
    Should work

--- src/projekt_del_2/TreesSort.java
+++ src/projekt_del_2/TreesSort.java
@@ -1,23 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package projekt_del_2;
 
 import java.util.Locale;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
  *
- * @author Borgar Bordoy
+ * This class uses DictBinTree methods to
+ * sort a list of integers that come from input
  */
+
 public class TreesSort {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        while (input.hasNextInt()) {
-
-        }
+        
+        List<Integer> inputNumbers = new ArrayList<Integer>();
+        Scanner scan = new Scanner(System.in);
+        
+        while (scan.hasNextInt())
+            
+            inputNumbers.add(scan.nextInt());
+        Dict dict = new DictBinTree();
+        
+        for (int numb : inputNumbers)
+            dict.insert(numb);
+        
+        for (int numb : dict.orderedTraversal())
+            System.out.println(numb);
+        
     }
-}
+    
+    }
+
