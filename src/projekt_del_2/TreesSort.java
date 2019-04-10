d04110fdf49c46e9bbe5224bbcece7dee1fbc7d8
Borgar Bordoy <bobor16@student.sdu.dk>
2019-04-10 14:43:35 +0200

    guide added to treesort

--- src/projekt_del_2/TreesSort.java
+++ src/projekt_del_2/TreesSort.java
@@ -1,4 +1,3 @@
-
 package projekt_del_2;
 
 import java.util.Locale;
@@ -8,29 +7,30 @@ import java.util.Scanner;
 
 /**
  *
- * This class uses DictBinTree methods to
- * sort a list of integers that come from input
+ * This class uses DictBinTree methods to sort a list of integers that come from
+ * input
  */
-
 public class TreesSort {
 
     public static void main(String[] args) {
-        
-        List<Integer> inputNumbers = new ArrayList<Integer>();
+
+        List<Integer> inputNumbers = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
-        
-        while (scan.hasNextInt())
-            
+        System.out.println("Type some integers seperated by space and end with ' ; ' ");
+        while (scan.hasNextInt()) {
             inputNumbers.add(scan.nextInt());
+        }
+        scan.close();
         Dict dict = new DictBinTree();
-        
-        for (int numb : inputNumbers)
+
+        for (int numb : inputNumbers) {
             dict.insert(numb);
-        
-        for (int numb : dict.orderedTraversal())
+        }
+        System.out.println("After calling the orderedTraversal method: ");
+        for (int numb : dict.orderedTraversal()) {
             System.out.println(numb);
-        
-    }
-    
+        }
+
     }
 
+}
