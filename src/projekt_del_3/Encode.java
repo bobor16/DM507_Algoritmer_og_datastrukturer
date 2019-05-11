1f1bd0f36e5c8cb6042854cee2942e7fb629c5cf
Borgar Bordoy <bobor16@student.sdu.dk>
2019-05-11 10:31:13 +0200

    Encode class

---
+++ src/projekt_del_3/Encode.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projekt_del_3;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.IntArrayData;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Borgar Bordoy
+ */
+public class Encode {
+
+    Scanner input = new Scanner(System.in);
+    private int[] intArray = new int[257];
+
+}
