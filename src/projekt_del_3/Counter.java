10f503355856ef5f2864edf9a4665c5650db4b9b
Rasmus Hansen <rasmush92@live.com>
2019-05-13 15:51:03 +0200

    FreqTable (Not done)

---
+++ src/projekt_del_3/Counter.java
@@ -0,0 +1,57 @@
+package projekt_del_3;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+
+
+public class Counter {
+
+    // java code that create ASCII frequency table from a text file
+    public static final int ASCII = 256;
+
+    public static void main(String[] args) {
+        try {
+            int[] frequency = createTable("C:\\Users\\rasmu\\OneDrive\\SDU\\4. Semester\\DM507 - Algoritmer og Datastrukturer\\Projects\\textFile.txt");
+
+
+            if (frequency.length == 0)
+                System.out.println("Error reading from file");
+            else {
+                for (int i = 32; i < ASCII; i++)
+                    System.out.println("ASCII code: " + i + " ,character: " + (char) i
+                            + " ,frequency: "
+                            + frequency[i]);
+
+                System.out.println("\nTotal characters in URL: " + sumTotal(frequency));
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static int sumTotal(int[] array) {
+        int total = 0;
+        for (int x : array) {
+            total += x;
+        }
+        return total;
+    }
+
+    public static int[] createTable(String fileName) throws FileNotFoundException, IOException {
+        int[] frequency = new int[ASCII];
+        File f = new File(fileName);
+        try (FileReader r = new FileReader(f)) {
+            while (r.ready()) {
+                int ch = r.read();
+                if (ch >= 0 && ch < frequency.length)
+                    frequency[ch]++;
+                else
+                    System.out.println("Not ASCII: " + ch + " " + (char) ch);
+            }
+        }
+        return frequency;
+
+    }
+}
