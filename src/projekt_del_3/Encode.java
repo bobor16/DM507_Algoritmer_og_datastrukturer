1b8dec9a83d2d36c190083385515914f3ffe5e91
Borgar Bordoy <bobor16@student.sdu.dk>
2019-05-14 16:15:09 +0200

    Huffman fix 100000000000000000

--- src/projekt_del_3/Encode.java
+++ src/projekt_del_3/Encode.java
@@ -11,6 +11,7 @@ import Projekt_del_I.PQHeap;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import projekt_del_2.DictBinTree;
 import projekt_del_2.Node;
 
 /**
@@ -21,33 +22,26 @@ public class Encode {
 
     public static final int ASCII = 256;
 //    private PQ queue;
-    private Node y;
-    private Node x;
 
     public static void main(String[] args) throws Exception {
         Encode encode = new Encode();
-        try {
-            int[] frequency = encode.createTable("src/textFile.txt");
+        int[] frequency = encode.createTable("src/textFile.txt");
 
-            if (frequency.length == 0) {
-                System.out.println("Error reading from file");
-            } else {
-                for (int i = 32; i < ASCII; i++) {
-                    if (frequency[i] > 0) {
-                        System.out.println("ASCII code: " + i + " character: " + (char) i
-                                + " frequency: "
-                                + frequency[i]);
-                    }
+        if (frequency.length == 0) {
+            System.out.println("Error reading from file");
+        } else {
+            for (int i = 32; i < ASCII; i++) {
+                if (frequency[i] > 0) {
+                    System.out.println("ASCII code: " + i + " character: " + (char) i
+                            + " frequency: "
+                            + frequency[i]);
                 }
-
-                System.out.println("\nTotal characters in URL: " + encode.sum(frequency));
-
-                encode.huffman(frequency);
             }
-        } catch (Exception e) {
-            System.out.println(e);
-        }
 
+            System.out.println("\nTotal characters in URL: " + encode.sum(frequency));
+
+            encode.huffman(frequency);
+        }
     }
 
     public int[] createTable(String fileName) throws FileNotFoundException, IOException {
@@ -74,20 +68,34 @@ public class Encode {
         return total;
     }
 
+    public String[] makeCode() {
+        String code = "";
+        DictBinTree d = new DictBinTree();
+
+        if (d.treeSearch(x, ASCII)) {
+
+        }
+    }
+
     public Element huffman(int[] C) {
+
         PQ Q = new PQHeap(C.length);
+        for (int o = 0; o < C.length - 1; o++) {
+            Node s = new Node(o);
+            s.setFreq(C[o]);
+            Q.insert(new Element(s.getFreq(), s));
+        }
+
         for (int i = 1; i < C.length - 1; i++) {
-//            if (C[i] > 0) {
+            Node x;
+            Node y;
             Node z = new Node(i);
             x = (Node) Q.extractMin().getData();
             y = (Node) Q.extractMin().getData();
             z.setLeftChild(x);
             z.setRightChild(y);
             z.setFreq(x.getFreq() + y.getFreq());
-            Q.insert((Element) Q);
-//            } else {
-//                System.out.println("fuck");
-//            }
+            Q.insert(new Element(z.getFreq(), z));
         }
         return Q.extractMin();
 
